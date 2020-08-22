bf113664027aaa53e5722a5700fa271a1927bb53
Joaquim <juakacc@gmail.com>
2020-08-22 14:02:38 -0300

    Adjust header with logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
+import Logo from '../../img/logo.svg';
+
 import './styles.css';
 
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <h1 className="grid-4">
-          <a href="/">Blog do Deusinho</a>
-        </h1>
+        <a href="/" className="grid-4 logo">
+          <img src={Logo} alt="Logo do Deusinho" />
+        </a>
 
-        <nav className="grid-10 menus">
+        <nav className="grid-12 menus">
           <ul>
             <li><a href="/sobre-mim">Sobre mim</a></li>
             <li><a href="/pergunte">Pergunte</a></li>
