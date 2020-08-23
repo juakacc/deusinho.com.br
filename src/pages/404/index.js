b8176b5f7fc5176b7b9f1f90c05d2d6606069948
Joaquim <juakacc@gmail.com>
2020-08-22 22:29:26 -0300

    Add page not found 404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -6,12 +6,23 @@ import '../../css/normalize.css';
 import '../../css/reset.css';
 import '../../css/grid.css';
 import '../../css/styles.css';
+import './styles.css';
+
+import NotFoundImage from '../../img/404.svg';
 
 export default function Home() {
   return (
     <PageDefault>
-      <h2>404 - Página não encontrada</h2>
-      <p>Desculpe, essa página não foi encontrada</p>
+      <div className="container not-found">
+        <h2 className="sub-title">404 - Página não encontrada</h2>
+
+        <div className="grid-4">
+          <img src={NotFoundImage} alt="Página não encontrada" />
+        </div>
+        <div className="grid-12">
+          <p>Desculpe, essa página não foi encontrada, verifique o endereço e tente novamente.</p>
+        </div>
+      </div>
     </PageDefault>
   );
 }
