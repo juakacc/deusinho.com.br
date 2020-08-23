cf9f8d84c4dc7d03fc4d0404390189056639346c
Joaquim <juakacc@gmail.com>
2020-08-23 12:48:59 -0300

    Create a screen of plan

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,14 +8,15 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
-        <a href="/" className="grid-4 logo">
+        <a href="/" className="grid-3 logo">
           <img src={Logo} alt="Logo do Deusinho" />
         </a>
 
-        <nav className="grid-12 menus">
+        <nav className="grid-13 menus">
           <ul>
             <li><a href="/sobre-mim">Sobre mim</a></li>
             <li><a href="/#askme">Pergunte</a></li>
+            <li><a href="/plano-de-governo">Plano de Governo</a></li>
           </ul>
         </nav>
       </div>
---
+++ src/pages/plano-de-governo/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PageDefault from '../../components/PageDefault';
+
+import './styles.css';
+
+function PlanoDeGoverno() {
+  return (
+    <PageDefault>
+      <div className="plane-cover" />
+
+      <div className="container">
+        <h2 className="sub-title">Plano de Governo</h2>
+      </div>
+
+      <div className="container">
+        <div className="grid-16">
+          <p>
+            Aqui serão listadas algumas de nossas proposta para Olivêdos
+            continuar no caminho certo.
+          </p>
+          <p>
+            O que você acha que é essencial para uma Olivêdos melhor?
+            Não deixe de comentar comigo!!
+          </p>
+        </div>
+      </div>
+    </PageDefault>
+  );
+}
+
+export default PlanoDeGoverno;
