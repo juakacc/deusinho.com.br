ca3a58ae6b2302b72b1faf357ffcae3cabee7090
Joaquim <juakacc@gmail.com>
2020-09-05 22:53:37 -0300

    set environment variables

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  apiUrl: 'https://deusinho-api.herokuapp.com',
-};
--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,12 +1,10 @@
 import fetch from 'cross-fetch';
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-import { apiUrl } from '../../config';
-
 // eslint-disable-next-line import/prefer-default-export
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: apiUrl,
+    uri: process.env.GATSBY_API_URI,
     fetch,
   }),
   cache: new InMemoryCache(),
